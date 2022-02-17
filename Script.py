86f0ae176843a276efb8ec511a3d73c79f725d9c
Mahmoud-Darweesh <mahmouddarweesh@hotmail.com>
2022-02-17 22:38:54 +0400

    Image to text beta done
    Used cv2 to read the image then used a char density to convert every pixel to a char then added it to a string row and appended that row to the result.txt

---
+++ Script.py
@@ -0,0 +1,37 @@
+#capture an image
+import cv2
+import math
+
+density = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1[]?-_+~<>i!lI;:,"^`                                                                           '
+
+resolutionWidth = 200
+resolutionHeight = 200
+
+targetImage = 'target.PNG'
+
+#capture the target image
+img = cv2.imread(targetImage, 0)
+
+#set image resolution
+img = cv2.resize(img,(resolutionWidth, resolutionHeight))
+
+def convert():
+    row = ''
+    for y in range(resolutionHeight):
+        print(row)
+        with open('result.txt', 'a') as f:
+            f.write(row + '\n')
+        row = ''
+        for x in range(resolutionWidth):
+            row += density[math.floor((img[y,x]/255)*(len(density)-1))]
+            row += '  '
+
+convert()
+
+#read each pixel
+
+cv2.imshow('output',img)
+cv2.waitKey(0)
+while True:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
